8e54e61d0f30a1e73446ff6b1ae08d88a92878aa
aaryan2134 <aaryan2134@gmail.com>
2022-04-24 00:19:23 +0530

    New database

---
+++ Database/dbms_project.sql
@@ -0,0 +1,283 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2022 at 08:47 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `leaderboard` ()  NO SQL
+select q.quizname,s.score,s.totalscore,st.name,s.mail from score s,student st,quiz q where s.mail=st.mail and q.quizid=s.quizid order by score DESC$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dept`
+--
+
+CREATE TABLE `dept` (
+  `dept_id` int(11) NOT NULL,
+  `dept_name` varchar(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dept`
+--
+
+INSERT INTO `dept` (`dept_id`, `dept_name`) VALUES
+(1, 'CSE'),
+(2, 'ISE'),
+(3, 'ECE'),
+(4, 'EEE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `qs` varchar(200) NOT NULL,
+  `op1` varchar(50) NOT NULL,
+  `op2` varchar(50) NOT NULL,
+  `op3` varchar(50) NOT NULL,
+  `answer` varchar(50) NOT NULL,
+  `quizid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`qs`, `op1`, `op2`, `op3`, `answer`, `quizid`) VALUES
+('A Database Management System is a type of _________software.', 'It is a kind of application software', 'It is a kind of general software', 'Both A and B', 'It is a type of system software', 9),
+('The term \"FAT\" is stands for_____', 'File Allocation Tree', 'All of the above', 'File Allocation Graph', 'File Allocation Table', 9),
+('What do you mean by one to many relationships?', 'One class may have many teachers', 'Many teachers may have many classes', 'Many classes may have many teachers', 'One teacher can have many classes', 8),
+('Which of the following is generally used for performing tasks like creating the structure of the relations, deleting relation?', 'DML(Data Manipulation Language)', 'Query', 'Relational Schema', 'DDL(Data Definition Language)', 8),
+('Which of the following provides the ability to query information from the database and insert tuples into, delete tuples from, and modify tuples in the database?', 'Relational Schema', 'Query', 'DDL(Data Definition Language)', 'DML(Data Manipulation Language)', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `quizid` int(11) NOT NULL,
+  `quizname` varchar(20) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `mail` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`quizid`, `quizname`, `date_created`, `mail`) VALUES
+(8, 'dbms lab', '2022-04-23 14:43:21', 'test@gmail.com'),
+(9, 'dbms lab test 2', '2022-04-23 14:54:18', 'test@gmail.com');
+
+--
+-- Triggers `quiz`
+--
+DELIMITER $$
+CREATE TRIGGER `ondeleteqs` AFTER DELETE ON `quiz` FOR EACH ROW delete from questions where questions.quizid=old.quizid
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `score`
+--
+
+CREATE TABLE `score` (
+  `slno` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `quizid` int(11) NOT NULL,
+  `mail` varchar(30) DEFAULT NULL,
+  `totalscore` int(11) DEFAULT NULL,
+  `remark` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `score`
+--
+
+INSERT INTO `score` (`slno`, `score`, `quizid`, `mail`, `totalscore`, `remark`) VALUES
+(28, 2, 8, 'absaar123@gmail.com', 3, 'good'),
+(29, 2, 9, 'absaar123@gmail.com', 2, 'good'),
+(30, 1, 8, 'absaar123@gmail.com', 3, 'good'),
+(31, 0, 8, 'naman@gmail.com', 3, 'bad'),
+(32, 3, 8, 'aaryan2134@gmail.com', 3, 'good'),
+(33, 0, 9, 'aaryan2134@gmail.com', 2, 'bad');
+
+--
+-- Triggers `score`
+--
+DELIMITER $$
+CREATE TRIGGER `remarks` BEFORE INSERT ON `score` FOR EACH ROW set NEW.remark = if(NEW.score = 0, 'bad', 'good')
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `staffid` varchar(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `mail` varchar(30) NOT NULL,
+  `phno` varchar(10) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `DOB` varchar(10) NOT NULL,
+  `pw` varchar(200) NOT NULL,
+  `dept` varchar(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`staffid`, `name`, `mail`, `phno`, `gender`, `DOB`, `pw`, `dept`) VALUES
+('190', 'Shailender Kumar', 'test2@gmail.com', '1234567895', 'M', '1980-01-01', 'abWMpd9uBwR.g', 'CSE'),
+('189', 'Prajjwal Kumar', 'test@gmail.com', '1234567897', 'M', '1999-01-01', 'abFxQCo2p0du.', 'CSE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `usn` varchar(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `mail` varchar(30) NOT NULL,
+  `phno` varchar(10) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `DOB` varchar(10) NOT NULL,
+  `pw` varchar(200) NOT NULL,
+  `dept` varchar(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`usn`, `name`, `mail`, `phno`, `gender`, `DOB`, `pw`, `dept`) VALUES
+('123', 'Aaryan', 'aaryan2134@gmail.com', '1234567890', 'M', '2001-10-02', 'abWMpd9uBwR.g', 'CSE'),
+('781', 'Absaar', 'absaar123@gmail.com', '1234567875', 'M', '2001-10-02', 'abWMpd9uBwR.g', 'CSE'),
+('566', 'Naman', 'naman@gmail.com', '8913113425', 'M', '2010-03-12', 'abWMpd9uBwR.g', 'ECE');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dept`
+--
+ALTER TABLE `dept`
+  ADD PRIMARY KEY (`dept_id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD UNIQUE KEY `qs` (`qs`),
+  ADD KEY `quizid` (`quizid`),
+  ADD KEY `quizid_2` (`quizid`),
+  ADD KEY `quizid_3` (`quizid`);
+
+--
+-- Indexes for table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`quizid`),
+  ADD KEY `mail` (`mail`);
+
+--
+-- Indexes for table `score`
+--
+ALTER TABLE `score`
+  ADD PRIMARY KEY (`slno`),
+  ADD KEY `quizid` (`quizid`),
+  ADD KEY `mail` (`mail`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`mail`),
+  ADD UNIQUE KEY `mail` (`mail`,`phno`),
+  ADD UNIQUE KEY `staffid` (`staffid`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`mail`),
+  ADD UNIQUE KEY `mail` (`mail`),
+  ADD UNIQUE KEY `phno` (`phno`),
+  ADD UNIQUE KEY `usn` (`usn`),
+  ADD KEY `dept` (`dept`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `quizid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `score`
+--
+ALTER TABLE `score`
+  MODIFY `slno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD CONSTRAINT `quiz_ibfk_1` FOREIGN KEY (`mail`) REFERENCES `staff` (`mail`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `score`
+--
+ALTER TABLE `score`
+  ADD CONSTRAINT `score_ibfk_1` FOREIGN KEY (`quizid`) REFERENCES `quiz` (`quizid`) ON DELETE CASCADE,
+  ADD CONSTRAINT `score_ibfk_2` FOREIGN KEY (`mail`) REFERENCES `student` (`mail`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
